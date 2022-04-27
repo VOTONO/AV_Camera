cb2431e66c35e216c024461d5537f2a98c7838a7
Виталий <miro.vitalya@yandex.ru>
2022-04-27 11:25:48 +0800

    Initial Commit

---
+++ AV_Camera/AV_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AV_CameraApp.swift
+//  AV_Camera
+//
+//  Created by Mac User on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AV_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
